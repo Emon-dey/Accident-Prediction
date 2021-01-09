50dadaab9e4c4002757fbed4368f9320ae0c659a
Emon-dey <emondey.eee.cuet@gmail.com>
2021-01-09 02:52:29 -0500

    base file

---
+++ base.py
@@ -0,0 +1,204 @@
+import os
+import sys
+import psutil
+
+import pandas as pd
+import numpy as np
+
+import keras
+import random
+
+from keras.models import Sequential,Model
+from keras.layers import Dense,LSTM,Activation,Dropout,BatchNormalization,Input,Embedding
+from keras.layers import Flatten,Conv2D,MaxPooling2D,Bidirectional,concatenate
+from numpy import zeros, newaxis
+
+# from keras.layers.convolutional import Convolution1D, MaxPooling1D, ZeroPadding2D
+from keras.layers.convolutional import Conv1D
+from keras.layers import Dense
+from keras.layers.convolutional import MaxPooling1D
+from keras.layers import Flatten
+from keras.utils import to_categorical
+from keras.callbacks import EarlyStopping
+from keras import regularizers
+from keras import optimizers
+import keras_metrics as km
+
+import matplotlib
+import matplotlib.pyplot as plt
+import math
+from multiprocessing import cpu_count,Pool 
+import multiprocessing
+
+
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
+
+from keras.utils import np_utils
+from sklearn.metrics import recall_score,precision_score,f1_score,accuracy_score, confusion_matrix
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import ShuffleSplit
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report
+
+from sklearn.metrics import make_scorer
+from keras.wrappers.scikit_learn import KerasClassifier
+
+
+
+
+from sklearn.metrics import roc_curve, auc
+
+from scipy import interp
+import matplotlib.pyplot as plt
+from itertools import cycle
+
+from keras_self_attention import SeqSelfAttention
+
+from utils import *
+
+SEQ=8 #sequence for LSTM
+
+verbose = 2
+dropout=0.2
+VAL_SPLIT = 0.2
+patience = 15
+lr=0.01
+weight_decay = 0.0000
+lr_decay=1e-6
+ADD_ON_LAYERS = True
+ACT_PRIOR = 'sigmoid'
+ACT_POSTERIOR = 'relu'
+LSTM_UNIT = 128
+GEOHASH_UNIT = 128
+EMBEDDING_UNIT = 128
+Embedding_outdim = 128
+NLP_UNIT = 128
+SEQ_UNIT = 256
+DENSE_CONCAT = 512
+CONV_UNIT = 32
+weights = np.array([1,1])
+
+class base_model(object):        
+    def __init__(self,n_jobs=10,act=ACT_POSTERIOR,city='Atlanta'): 
+        self.n_jobs=n_jobs
+        self.CITY=city
+        self.act=act
+   
+    def load_data(self,category=None,with_geocode=False):
+        self.X_train = np.load('X_train_'+self.CITY+'.npy')
+        self.y_train = np.load('y_train_'+self.CITY+'.npy')
+        self.X_test = np.load('X_test_'+self.CITY+'.npy')
+        self.y_test = np.load('y_test_'+self.CITY+'.npy')
+        
+        if not with_geocode:
+            self.X_train = self.X_train[:,0:-1]    
+            self.X_test = self.X_test[:,0:-1]     
+        self.update_y()   
+        
+        if category!=None:
+            l_train=[]
+            l_test=[]
+            for cat in category:
+                l_train.append(reshape_cat(self.X_train,cat))
+                l_test.append(reshape_cat(self.X_test,cat))
+            self.X_train = np.concatenate(l_train,axis=1)
+            self.X_test = np.concatenate(l_test,axis=1)
+        
+        print ('load and test: shapes for train and test, X/Y')
+        print (self.X_train.shape)
+        print (self.y_train.shape)
+        print (self.X_test.shape)
+        print (self.y_test.shape)
+    
+    def update_y(self):
+        self.y_train = to_categorical(self.y_train,2)
+        self.y_test = to_categorical(self.y_test,2)
+    def last_layers(self,model_in):
+        model_in = Conv1D(32,3,
+                    kernel_regularizer=regularizers.l2(self.weight_decay),
+                    activation=self.act, input_shape=(model_in.shape))(model_in)
+#         model_in = MaxPooling1D(pool_size=3,strides=None, padding="valid")
+        print(model_in.shape)
+        
+        model_in = Dense(DENSE_CONCAT,
+                    kernel_regularizer=regularizers.l2(self.weight_decay),
+                    activation=self.act)(model_in)
+        
+        model_in = Dense(units=int(DENSE_CONCAT/2),
+                    kernel_regularizer=regularizers.l2(self.weight_decay),
+                    activation=None)(model_in)
+        if ADD_ON_LAYERS:
+            model_in = BatchNormalization()(model_in)
+        model_in = Activation(self.act)(model_in)
+        model_in = Dropout(dropout)(model_in)
+        
+        model_in = Dense(units=int(DENSE_CONCAT/8),
+                    kernel_regularizer=regularizers.l2(self.weight_decay),
+                    activation=None)(model_in)
+        if ADD_ON_LAYERS:
+            model_in = BatchNormalization()(model_in)
+        model_in = Activation(self.act)(model_in)
+        #model_in = Dropout(dropout)(model_in)
+        ######################################
+        main_output = Dense(self.output_dim, activation=self.activation)(model_in)
+        return main_output
+        
+class keras_model(base_model):
+    def __init__(self,city='Atlanta',activation='softmax',batch_size=128, epoch =40 ,n_jobs=1,act=ACT_POSTERIOR):
+        super(keras_model,self).__init__(act=act,city=city)
+        self.output_dim = 2
+        self.activation=activation
+        self.batch_size = batch_size
+        self.epoch = epoch
+        self.n_jobs=n_jobs
+        self.weight_decay = weight_decay
+        self.lr=lr
+        self.lr_decay=lr_decay
+        
+    def reshape(self,x):
+        x = x[:,0:-114]
+        x = x.reshape((x.shape[0],SEQ,int(x.shape[1]/SEQ)))
+        return x
+    
+    def compile_model(self,model=None):
+        #precision = km.categorical_precision(label=1)
+        #recall = km.categorical_recall(label=1)
+        f1_score = km.categorical_f1_score(label=1)
+        self.earlyStopping = EarlyStopping(monitor='val_f1_score', 
+                                           #monitor = 'val_categorical_f1_score',
+                                           restore_best_weights=True,
+                                           patience=patience, verbose=0, mode='max'#,baseline=0.1
+                                          )
+        adam  = optimizers.Adam(lr=self.lr, decay=self.lr_decay)
+        loss=weighted_categorical_crossentropy(weights)
+        self.model.compile(optimizer=adam, loss=loss#'categorical_crossentropy'
+                           ,metrics=['accuracy'])
+    def create_model(self):
+        self.model = KerasClassifier(build_fn=self.build_model, epochs=self.epoch, batch_size=self.batch_size, verbose=1)
+    def make_report(self,city,y_true,y_pred):
+        data_frame = classification_report(y_true.argmax(axis=-1), y_pred.argmax(axis=-1),output_dict=True)
+        df = pd.DataFrame(data_frame)
+        df = df.reset_index()
+        roc_dict = self.roc_auc(city,y_true,y_pred)
+        df = df.append({'index':'auc','0': roc_dict[0], '1': roc_dict[1],
+                   'micro avg': roc_dict['micro'],
+                   'macro avg':roc_dict['macro']}, ignore_index=True)
+        df = df.set_index('index')
+        display(df)
+        return df
+    
+    def confusion(self,city,y_true,y_pred):
+        data_frame1 = confusion_matrix(y_true.argmax(axis=-1), y_pred.argmax(axis=-1))
+        df1 = pd.DataFrame(data_frame1)
+        
+      
+        display(df1)
+        return df1
+        
+    def roc_auc(self,city,y_test,y_score):
+        fpr,tpr,roc_auc = roc_auc_compute(city,y_test,y_score)
+        plot_roc(city,fpr,tpr,roc_auc)
+        return roc_auc
+         
+        
